cb3feebb580c8ba4c9bbca0c1d039bba2ac33e7d
Tobias Ouwejan <tobias.ouwejan@sap.com>
2018-12-21 14:09:02 +0100

    refactor pagination

--- src/app/pagination/pagination.component.ts
+++ src/app/pagination/pagination.component.ts
@@ -10,25 +10,31 @@ import { ContactsService } from '../contacts/contacts.service';
 export class PaginationComponent implements OnInit {
   @Input() pagination: HyPaginationPagination;
 
-  pageLength: number;
+  // pageLength: number;
   pages = [];
 
   constructor(private contactService: ContactsService) {}
 
   ngOnInit() {
-    this.pageLength = Math.round(
-      (this.pagination.totalcount + this.pagination.size / 2) /
-        this.pagination.size
-    );
     for (let i = 0; i < this.pageLength; i++) {
       this.pages.push(i + 1);
     }
   }
 
-  openPage(page: number) {
-    if (page <= 0 && page > this.pageLength) return;
+  get pageLength() {
+    return Math.round(
+      (this.pagination.totalcount + this.pagination.size / 2) /
+        this.pagination.size
+    );
+  }
 
+  set page(page: number) {
+    if (page <= 0 || page > this.pageLength) return;
     this.contactService.page$.next(page);
+  }
+
+  set size(size: number) {
+    this.contactService.size$.next(size);
   }
 
   get hasNext() {
